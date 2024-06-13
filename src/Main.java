27a6651ef10bf220ec165a4dd6884d7ad21533b7
katros01 <kairos.mutabaruka@amalitechtraining.org>
2024-06-13 14:56:16 +0200

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     static  int maxGrade (int[] a) {
         int max =a[0];
